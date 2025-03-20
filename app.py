e41ea68b6f31c72340515f3055e1cf0d41030912
Manish-Gritbyte <manish@gritbyte.com>
2025-03-20 17:41:50 +0530

    v3

--- app.py
+++ app.py
@@ -127,5 +127,5 @@ def download_excel():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
